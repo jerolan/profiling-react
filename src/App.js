9e6ed50cbf3cff3102db0d16915db9d2f4a59b6f
Jerome Olvera <jolveras@gbm.com.mx>
2022-05-17 22:45:12 -0500

    base layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="tracking-tight">
+      <header className="flex items-center justify-between h-16 border px-16">
+        <h1 className="font-bold">super awesome</h1>
+        <figure>
+          <div className="rounded-full bg-indigo-600 h-8 w-8" />
+        </figure>
       </header>
-    </div>
+      <div className="px-16 py-32">
+        <div>
+          <h4 className="text-2xl font-bold text-gray-900 pb-8">My Products</h4>
+          <div className="grid grid-cols-4 gap-y-10 gap-x-6">
+            <article className="group relative">
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 h-80">
+                <div className="w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-0"
+                    ></span>
+                    Basic Tee
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">Black</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">$35</p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
